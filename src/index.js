440527e7162e8069efdbf63079f7812cb2271ff1
Vedanttttt05 <vedantpatil05052004@gmail.com>
2026-02-02 00:22:49 +0530

    express server basic setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 
+import { app } from "./app.js";
 
 import connectDB from "./db/index.js";
 
-connectDB();
-
+connectDB().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+     
+    app.on("error", (err) => {
+        console.error("Server error", err);
+    });
+}).catch((err) => {
+    console.error("Failed to connect to the database", err);
+});
