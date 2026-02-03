823c0f2c4d31ada0cd36adc5b8f30c9300445142
Vedanttttt05 <vedantpatil05052004@gmail.com>
2026-02-04 00:47:50 +0530

    add user model

---
+++ src/models/user.model.js
@@ -0,0 +1,107 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+
+const userSchema = new Schema({
+    role : { type: String, enum: ["user" , "admin"] , default : "user"},
+
+    username: { type: String, required: true, unique: true , lowercase: true, trim: true , index: true},
+
+    email: { type: String, required: true , lowercase: true, trim: true },
+
+    isEmailVerified : { type : Boolean , default : false},
+
+    fullName: { type: String, required: true , trim: true  , index :true},
+
+    avatar: { type: String },
+
+    avatarPublicId: { type: String },
+
+    watchHistory: [{ type: Schema.Types.ObjectId, ref: "Video" }],   
+
+    password: { type: String, select: false, required: function(){
+        return this.provider === "local";
+    } },
+
+    refreshToken: { type: String  , password : false },
+
+    lastLoginAt : { type : Date} ,
+
+    provider :{
+        type : String,
+        enum : ["local" , "google" , "facebook"],
+        default : "local"
+    },
+
+    providerId : {
+        type : String ,
+        required : function(){
+            return this.provider !== "local";
+        }   
+    },
+
+    loginAttempts : { type : Number , default : 0 },
+
+    lockUntil : { type : Date } ,
+
+     passwordResetToken : { type : String , select : false } ,
+
+     passwordResetExpires : { type : Date  }
+
+}, { timestamps: true });
+
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password") || !this.password) {
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.index({ email: 1, provider: 1 }, { unique: true });
+userSchema.index({ username: 1 }, { unique: true });
+
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);    
+};
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+        userId: this._id, 
+        role : this.role
+    } ,process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+    )}; 
+
+
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+        userId: this._id,
+
+
+    } ,process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+    )
+
+};
+
+
+userSchema.virtual("isLocked").get(function () {
+  return !!(this.lockUntil && this.lockUntil > Date.now());
+});
+
+
+
+const User = mongoose.model("User", userSchema);
+
+export {User};
