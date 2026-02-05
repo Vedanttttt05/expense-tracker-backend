dab71aad6ab635d2585016dbb80442fa0ed68a32
Vedanttttt05 <vedantpatil05052004@gmail.com>
2026-02-06 02:47:43 +0530

    add get transactions controller

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -1,6 +1,5 @@
 import {asyncHandler} from '../utils/asyncHandler.js';
 import apiError from '../utils/apiError.js';
-import { User } from '../models/user.model.js';
 import { Transaction } from '../models/transaction.model.js';
 import  apiResponse  from '../utils/apiResponse.js';
 import mongoose from "mongoose";
@@ -32,4 +31,35 @@ const createTransaction = asyncHandler (async (req, res) => {
 
     res.status(201).json(new apiResponse(201, "Transaction created successfully", transaction));
 });
+
+const getTransactions = asyncHandler (async (req, res) => {
+    const { page = 1, limit = 10, type, category, startDate, endDate } = req.query;
+    const filters = { user: req.user._id, isDeleted: false };
+    if (type) {
+        if (!["income", "expense"].includes(type)) {
+            throw new apiError(400, "Type filter must be either 'income' or 'expense'");
+        }
+        filters.type = type;
+    }
+    if (category) {
+        filters.category = category;
+    }
+    if (startDate || endDate) {
+        filters.date = {};
+        if (startDate) {
+            filters.date.$gte = new Date(startDate);
+        }
+        if (endDate) {
+            filters.date.$lte = new Date(endDate);
+        }
+    }
+    const transactions = await Transaction.find(filters)
+        .sort({ date: -1 })
+        .skip((page - 1) * limit)
+        .limit(parseInt(limit));
+    const total = await Transaction.countDocuments(filters);
+    res.status(200).json(new apiResponse(200, "Transactions retrieved successfully", { transactions, total }));
+});
+
+
 
